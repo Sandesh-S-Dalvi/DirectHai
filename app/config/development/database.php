9c78f7f540148665f530dd3c51087eacb6bd5838
sandesh <dalvi.sandesh@gmail.com>
2014-08-04 00:47:32 +0530

    Configured redis for Pagodabox

--- app/config/development/database.php
+++ app/config/development/database.php
@@ -1,9 +1,31 @@
 <?php
 
 return array(
+
+/*
+    |--------------------------------------------------------------------------
+    | Database Connections
+    |--------------------------------------------------------------------------
+    |
+    | Here are each of the database connections setup for your application.
+    | Of course, examples of configuring each database platform that is
+    | supported by Laravel is shown below to make development simple.
+    |
+    |
+    | All database work in Laravel is done through the PHP PDO facilities
+    | so make sure you have the driver for your particular database of
+    | choice installed on your machine before you begin development.
+    |
+*/
+
+
+
     'profile' => false,
+
     'default' => 'development',
+
     'connections' => array(
+
         'development' => array(
             'driver'   => 'mysql',
             'host'     => $_SERVER['DB1_HOST'],
@@ -14,4 +36,29 @@ return array(
             'collation' => 'utf8_unicode_ci',
             'prefix'   => '',
         ))
+
+/*
+    |--------------------------------------------------------------------------
+    | Redis Databases
+    |--------------------------------------------------------------------------
+    |
+    | Redis is an open source, fast, and advanced key-value store that also
+    | provides a richer set of commands than a typical key-value systems
+    | such as APC or Memcached. Laravel makes it easy to dig right in.
+    |
+*/
+
+    'redis' => array(
+
+        'cluster' => false,
+
+        'default' => array(
+            'host'     => $_SERVER['CACHE1_HOST'],
+            'port'     => $_SERVER['CACHE1_PORT'],  
+            'database' => 0,
+        ),
+
+    ),
+
+
 );
